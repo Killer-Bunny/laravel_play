54686a04137f916192ba4a9b5c0e62465ea65b82
No name <shmuelonppp@gmail.com>
2020-05-16 15:09:40 +0300

    first backup

---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Bookable;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Route::get('bookables', function(Request $request) {
+//   return Bookable::all();
+// });
+//
+// Route::get('bookables/{id}', function(Request $request, $bookableId) {
+//   return Bookable::findOrFail($bookableId);
+// });
+
+Route::get('bookables', 'Api\BookableController@index');
+Route::get('bookables/{id}','Api\BookableController@show');
