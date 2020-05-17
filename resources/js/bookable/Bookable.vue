b85bb8526e89cf01bee0de778b3bb9cf8f1ababd
No name <shmuelonppp@gmail.com>
2020-05-17 14:39:05 +0300

    Added availability component

--- resources/js/bookable/Bookable.vue
+++ resources/js/bookable/Bookable.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row">
-    <div class="col-md-8">
+    <div class="col-md-8 pb-4">
       <div class="card">
         <div class="card-body">
           <div v-if="!loading">
@@ -16,14 +16,18 @@
         </div>
       </div>
     </div>
-    <div class="col-md-4">
-      AVAILABILITY & PRICES
+    <div class="col-md-4 pb-4">
+      <availability></availability>
     </div>
   </div>
 </template>
 
 <script type="text/javascript">
+  import Availability from "./Availability";
   export default {
+    components: {
+      Availability
+    },
     data() {
       return {
         bookable: null,
@@ -36,7 +40,7 @@
       axios
         .get(`/api/bookables/${this.$route.params.id}`)
         .then(response => {
-          this.bookable = response.data.data;
+          this.bookable = response.data;
           this.loading = false;
         });
     }
--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -50,22 +50,14 @@
       created() {
         this.loading = true;
 
-        // const p = new Promise((resolve, reject) => {
-        //   console.log(resolve);
-        //   console.log(reject);
-        //   setTimeout(() => resolve("hello"), 3000);
-        // }).then(result => console.log(`yellow world ${result}`))
-        // .catch(result => console.log(`Error ${result}`));
-        // console.log(p);
-
         const request = axios
           .get("/api/bookables")
           .then(response => {
-            this.bookables = response.data.data;
+            this.bookables = response.data;
             this.loading = false;
           });
 
-        //console.log(request);
+
       }
     }
 </script>
